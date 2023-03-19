34986de08564ae4794633cb7665c413d34377bde
amankashyap004 <amankashyap004@gmail.com>
2023-03-19 13:49:11 +0530

    fix bugs

--- src/components/ButtonBox.js
+++ src/components/ButtonBox.js
@@ -5,21 +5,13 @@ function ButtonBox() {
       <div className="btn-box">
          <div className="btn-container">
             <button type="submit" className="btn btn-email">
-<<<<<<< HEAD
                <i className="fa-solid fa-envelope btn-icon"></i>
-=======
-               <i class="fa-solid fa-envelope btn-icon"></i>
->>>>>>> 56d537b246e6dfe1541a6dba3368e93fe7bdcd21
                Email
             </button>
          </div>
          <div className="btn-container">
             <button type="submit" className="btn btn-linkedIn">
-<<<<<<< HEAD
                <i className="fa-brands fa-linkedin-in btn-icon"></i>
-=======
-               <i class="fa-brands fa-linkedin-in btn-icon"></i>
->>>>>>> 56d537b246e6dfe1541a6dba3368e93fe7bdcd21
                LinkedIn
             </button>
          </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,39 +5,24 @@ function Footer() {
       <footer className="footer">
          <a href="/">
             <div className="footer-contain">
-<<<<<<< HEAD
                <i className="fa-brands fa-twitter"></i>
-=======
-               <i class="fa-brands fa-twitter"></i>
->>>>>>> 56d537b246e6dfe1541a6dba3368e93fe7bdcd21
             </div>
          </a>
          <a href="/">
             <div className="footer-contain">
                {/* <i class="fa-brands fa-square-facebook"></i> */}
-<<<<<<< HEAD
                <i className="fa-brands fa-facebook-f"></i>
-=======
-               <i class="fa-brands fa-facebook-f"></i>
->>>>>>> 56d537b246e6dfe1541a6dba3368e93fe7bdcd21
             </div>
          </a>
          <a href="./">
             <div className="footer-contain">
-<<<<<<< HEAD
                <i className="fa-brands fa-instagram"></i>
-=======
-               <i class="fa-brands fa-instagram"></i>
->>>>>>> 56d537b246e6dfe1541a6dba3368e93fe7bdcd21
             </div>
          </a>
          <a href="https://github.com/amankashyap004" target="_blank">
             <div className="footer-contain last-contain">
-<<<<<<< HEAD
+               {" "}
                <i className="fa-brands fa-github"></i>
-=======
-               <i class="fa-brands fa-github"></i>
->>>>>>> 56d537b246e6dfe1541a6dba3368e93fe7bdcd21
             </div>
          </a>
       </footer>
